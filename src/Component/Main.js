ed40949ca49672f233af74318146fe22a08cfdc7
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-09-05 09:28:27 +0530

    add some commands

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import data from "./JsonFile/data.json";
-import audioFile from "../Audio/start.mp3";
 
 const Main = () => {
   const [transcript, setTranscript] = useState("");
@@ -9,9 +8,15 @@ const Main = () => {
     new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
   );
   const [date, setDate] = useState(() => new Date().toLocaleDateString());
+  const [todaysDay, setTodaysDay] = useState(() => {
+    const today = new Date();
+    const options = { weekday: "long" }; // You can use 'short' or 'narrow' for different formats
+    return today.toLocaleDateString(undefined, options);
+  });
   const [dynamicState, setDynamicState] = useState({
     time: time,
     date: date,
+    day: todaysDay,
   });
 
   useEffect(() => {
@@ -35,44 +40,49 @@ const Main = () => {
   }, []);
 
   const handleChange = () => {
-    // const audio = new Audio(audioFile);
-    // audio.play();
     const recognition = new (window.SpeechRecognition ||
       window.webkitSpeechRecognition)();
 
     recognition.onresult = (event) => {
       const currentTranscript = event.results[0][0].transcript.toLowerCase();
       console.log(currentTranscript);
-      jsonData &&
-        jsonData.find((val) => {
-          const voices = window.speechSynthesis.getVoices();
-          const selectedVoice = voices[0];
-          if (val.command.includes(currentTranscript)) {
-            if (val.command.includes(val.dynamicCommand)) {
-              setTranscript(dynamicState[val.dynamicCommand]);
-              if (selectedVoice) {
-                const dynamicVoiceOutPut = new SpeechSynthesisUtterance(
-                  dynamicState[val.dynamicCommand]
-                );
-                dynamicVoiceOutPut.voice = selectedVoice;
-                window.speechSynthesis.speak(dynamicVoiceOutPut);
-              }
-            } else {
-              setTranscript(val.outPut);
-              if (selectedVoice) {
-                const voiceOutPut = new SpeechSynthesisUtterance(val.outPut);
-                voiceOutPut.voice = selectedVoice;
-                window.speechSynthesis.speak(voiceOutPut);
-              }
-              if (val.openUrl) {
-                alert();
-                window.open(val.openUrl, "_blank"); // Open URL in a new tab
-              }
+      const match = jsonData.find((val) => {
+        const voices = window.speechSynthesis.getVoices();
+        const selectedVoice = voices[0];
+        if (val.command.includes(currentTranscript)) {
+          if (val.command.includes(val.dynamicCommand)) {
+            setTranscript(dynamicState[val.dynamicCommand]);
+            if (selectedVoice) {
+              const dynamicVoiceOutPut = new SpeechSynthesisUtterance(
+                dynamicState[val.dynamicCommand]
+              );
+              dynamicVoiceOutPut.voice = selectedVoice;
+              window.speechSynthesis.speak(dynamicVoiceOutPut);
+            }
+          } else {
+            setTranscript(val.outPut);
+            if (selectedVoice) {
+              const voiceOutPut = new SpeechSynthesisUtterance(val.outPut);
+              voiceOutPut.voice = selectedVoice;
+              window.speechSynthesis.speak(voiceOutPut);
+            }
+            if (val.openUrl) {
+              window.open(val.openUrl, "_blank");
             }
           }
-          return null; // You need to return something from the map function
-        });
+          return true;
+        }
+        return false;
+      });
+
+      if (!match) {
+        const dynamicVoiceOutPut = new SpeechSynthesisUtterance(
+          "Sorry I dont Know"
+        );
+        window.speechSynthesis.speak(dynamicVoiceOutPut);
+      }
     };
+
     recognition.start();
   };
 
